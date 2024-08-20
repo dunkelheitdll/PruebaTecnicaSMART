d92ade25492313bcd800d636df96d26e78c4caea
dunkelheitdll <64619948+dunkelheitdll@users.noreply.github.com>
2024-08-20 10:29:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,25 @@
-provider "kubernetes" {
-  config_path = "~/.kube/config"
+provider "azurerm" {
+  features = {}
 }
 
-resource "kubernetes_deployment" "prueba" {
-  metadata {
-    name = "prueba-deployment"
-    labels = {
-      app = "prueba"
-    }
+resource "azurerm_resource_group" "example" {
+  name     = "rg-cicd-pipeline"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "aks-cicd-cluster"
+  location            = "East US2"
+  resource_group_name = "Prueba"
+  dns_prefix          = "cicdpipeline"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
   }
 
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = "prueba"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "prueba"
-        }
-      }
-
-      spec {
-        container {
-          image = "pruebatecnica.azurecr.io/mi-repo:latest"
-          name  = "prueba"
-        }
-      }
-    }
+  identity {
+    type = "SystemAssigned"
   }
 }
