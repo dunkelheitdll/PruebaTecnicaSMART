d74a61a57e71b52dd607a4509b4d4535e2f90a98
dunkelheitdll <64619948+dunkelheitdll@users.noreply.github.com>
2024-08-20 01:31:21 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -11,3 +11,12 @@ def say_hello():
     Esta función no toma ningún argumento y no retorna ningún valor.
     """
     print("Hello, World!")
+
+"""
+Validacion del codigo para pruebas unitarias
+"""
+if __name__ == "__main__":
+    """
+    Retorna valor que será validado
+    """
+    print(say_hello())
